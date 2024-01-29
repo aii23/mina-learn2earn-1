6710a5fe0ef99ddcca41449bedb1b06a1fa9247d
Aleksandr Ivlev <a.ivlev.om@gmail.com>
2024-01-29 12:09:10 +0300

    Done(no testing)

--- src/AccountManager.ts
+++ src/AccountManager.ts
@@ -1,5 +1,6 @@
 import {
   Field,
+  MerkleMap,
   MerkleMapWitness,
   Poseidon,
   PublicKey,
@@ -10,6 +11,7 @@ import {
   method,
   state,
 } from 'o1js';
+import { Gadgets } from 'o1js/dist/node/lib/gadgets/gadgets';
 
 enum AccountState {
   NotListed,
@@ -37,11 +39,25 @@ export class AccountManager extends SmartContract {
   @state(PublicKey) administrator = State<PublicKey>();
   @state(UInt64) unusedAccounts = State<UInt64>();
 
+  @method init() {
+    super.init();
+
+    this.root.set(new MerkleMap().getRoot());
+  }
+
+  @method setAdministrator(administrator: PublicKey) {
+    const curAdministrator = this.administrator.getAndRequireEquals();
+    curAdministrator.assertEquals(PublicKey.empty());
+
+    this.administrator.set(administrator);
+  }
+
   @method addAccount(address: PublicKey, path: MerkleMapWitness) {
     // Check for administrator
+    const administrator = this.administrator.getAndRequireEquals();
+    administrator.assertEquals(this.sender);
 
     const root = this.root.getAndRequireEquals();
-
     const accountHash = Poseidon.hash(address.toFields());
 
     const [rootBefore, key] = path.computeRootAndKey(Field.from(0)); // No values yet
@@ -49,7 +65,7 @@ export class AccountManager extends SmartContract {
     root.assertEquals(rootBefore);
     key.assertEquals(accountHash);
 
-    const [newRoot, _] = path.computeRootAndKey(Account.listed().hash());
+    const [newRoot] = path.computeRootAndKey(Account.listed().hash());
 
     this.root.set(newRoot);
   }
@@ -57,10 +73,33 @@ export class AccountManager extends SmartContract {
   @method addMessage(path: MerkleMapWitness, message: Field) {
     // Check message for flags
 
+    {
+      let f1 = Gadgets.and(message, Field.from(1), 1).equals(Field.from(1));
+      let f2 = Gadgets.and(message, Field.from(1 << 1), 2).equals(
+        Field.from(1 << 1)
+      );
+      let f3 = Gadgets.and(message, Field.from(1 << 2), 3).equals(
+        Field.from(1 << 2)
+      );
+      let f4 = Gadgets.and(message, Field.from(1 << 3), 4).equals(
+        Field.from(1 << 3)
+      );
+      let f5 = Gadgets.and(message, Field.from(1 << 4), 5).equals(
+        Field.from(1 << 4)
+      );
+      let f6 = Gadgets.and(message, Field.from(1 << 5), 6).equals(
+        Field.from(1 << 5)
+      );
+      // First rule
+      f1.not().or(f2.or(f3).or(f4).or(f5).or(f6).not()).assertTrue();
+      // Second Rule
+      f2.not().or(f3).assertTrue();
+      // Third rule
+      f4.not().or(f5.not().and(f6.not())).assertTrue();
+    }
+
     const root = this.root.getAndRequireEquals();
-
     const accountHash = Poseidon.hash(this.sender.toFields());
-
     const [rootBefore, key] = path.computeRootAndKey(Account.listed().hash());
 
     root.assertEquals(rootBefore);
@@ -72,7 +111,6 @@ export class AccountManager extends SmartContract {
     });
 
     const [newRoot] = path.computeRootAndKey(newValue.hash());
-
     this.root.set(newRoot);
   }
 }
