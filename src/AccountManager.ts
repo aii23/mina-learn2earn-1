0832aff75c2d9fedc4bf30925e5c1540e3690824
Aleksandr Ivlev <a.ivlev.om@gmail.com>
2024-01-29 11:37:17 +0300

    Redo via struct

--- src/AccountManager.ts
+++ src/AccountManager.ts
@@ -5,6 +5,7 @@ import {
   PublicKey,
   SmartContract,
   State,
+  Struct,
   UInt64,
   method,
   state,
@@ -16,9 +17,23 @@ enum AccountState {
   Participated,
 }
 
+class Account extends Struct({
+  state: Field,
+  message: Field,
+}) {
+  static listed(): Account {
+    return new Account({
+      state: Field.from(AccountState.Listed),
+      message: Field.from(0),
+    });
+  }
+  hash(): Field {
+    return Poseidon.hash([this.state, this.message]);
+  }
+}
+
 export class AccountManager extends SmartContract {
   @state(Field) root = State<Field>();
-  @state(Field) messageRoot = State<Field>();
   @state(PublicKey) administrator = State<PublicKey>();
   @state(UInt64) unusedAccounts = State<UInt64>();
 
@@ -29,54 +44,35 @@ export class AccountManager extends SmartContract {
 
     const accountHash = Poseidon.hash(address.toFields());
 
-    const [rootBefore, key] = path.computeRootAndKey(
-      Field.from(AccountState.NotListed)
-    );
+    const [rootBefore, key] = path.computeRootAndKey(Field.from(0)); // No values yet
 
     root.assertEquals(rootBefore);
     key.assertEquals(accountHash);
 
-    const [newRoot, _] = path.computeRootAndKey(
-      Field.from(AccountState.Listed)
-    );
+    const [newRoot, _] = path.computeRootAndKey(Account.listed().hash());
 
     this.root.set(newRoot);
   }
 
-  @method addMessage(
-    path: MerkleMapWitness,
-    messagePath: MerkleMapWitness,
-    message: Field
-  ) {
+  @method addMessage(path: MerkleMapWitness, message: Field) {
     // Check message for flags
 
     const root = this.root.getAndRequireEquals();
-    const messageRoot = this.messageRoot.getAndRequireEquals();
 
     const accountHash = Poseidon.hash(this.sender.toFields());
 
-    const [rootBefore, key] = path.computeRootAndKey(
-      Field.from(AccountState.Listed)
-    );
+    const [rootBefore, key] = path.computeRootAndKey(Account.listed().hash());
 
     root.assertEquals(rootBefore);
     key.assertEquals(accountHash);
 
-    const [messageRootBefore, messageKey] = path.computeRootAndKey(
-      Field.from(0)
-    );
+    const newValue = new Account({
+      state: Field.from(AccountState.Participated),
+      message,
+    });
 
-    messageRoot.assertEquals(messageRootBefore);
-    messageKey.assertEquals(accountHash);
-
-    const [newRoot] = path.computeRootAndKey(
-      Field.from(AccountState.Participated)
-    );
+    const [newRoot] = path.computeRootAndKey(newValue.hash());
 
     this.root.set(newRoot);
-
-    // const [newMessageRoot, _] = path.computeRootAndKey(
-    //     Field.from(AccountState.Participated)
-    //   );
   }
 }
